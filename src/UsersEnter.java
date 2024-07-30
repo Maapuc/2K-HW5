e2f07253b487581a6d7e16d0dd90a067b515cc36
Nikolai <pojarka754@gmail.com>
2024-07-30 16:50:35 +0500

    Init

--- src/UsersEnter.java
+++ src/UsersEnter.java
@@ -2,30 +2,22 @@ public class UsersEnter {
     public static final String CHECKSUMBOLS = "qwertyuiopasdfghjklzxcvbnm1234567890_";
 
     public static void checkUsers(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        if (login.length() > 20 || !checkLogin(login)) {
+        if (login.length() > 20 || !checkString(login)) {
             throw new WrongLoginException();
         }
 
-        if (password.length() > 20 || !checkPassword(password) || !password.equals(confirmPassword)) {
+        if (password.length() > 20 || !checkString(password) || !password.equals(confirmPassword)) {
             throw new WrongPasswordException();
         }
     }
 
-    private static boolean checkLogin(String login) {
-        for (int i = 0; i < login.length(); i++) {
-            if (!CHECKSUMBOLS.contains(String.valueOf(login.charAt(i)))) {
+    private static boolean checkString(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (!CHECKSUMBOLS.contains(String.valueOf(str.charAt(i)))) {
                 return false;
             }
         }
         return true;
     }
 
-    private static boolean checkPassword(String password) {
-        for (int i = 0; i < password.length(); i++) {
-            if (!CHECKSUMBOLS.contains(String.valueOf(password.charAt(i)))) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
